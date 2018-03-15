da19a98ee5a60d7035e63d51ca2794bad85b53c2
Dylan Gonzalez <dylan.gonzalez@pwc-digital.ch>
2018-03-15 15:32:24 +0100

    Removed unused code for development

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -7,22 +7,10 @@
                     <div class="gallery__folder-hover">
                         <h2 class="gallery__folder-title">{{ folder.fields.title }}</h2>
                     </div>
-                    <!--<p>{{ folder.fields.description }}</p>-->
-                    <!--<p>{{ folder.fields.coverImage.fields.url }}</p>-->
-                    <!--<p>{{ folder.fields.coverImage.fields.file.url }}</p>-->
                     <img :src="folder.fields.coverImage.fields.file.url" class="gallery__folder-coverimage">
-                    <!--<p>{{ folder.fields.images.length }}</p>-->
-                    <!--<p>{{ folder.sys.id }}</p>-->
                 </div>
             </div>
         </div>
-        <!--<p>Get Folders:</p>-->
-        <!--<p>{{ getFolders }}</p>-->
-        <!--<div v-for="folder in getFolders">-->
-            <!--<p>Folder</p>-->
-        <!--</div>-->
-        <!--<p>fefe</p>-->
-        <!--<div v-on:click="loadPictures">Click</div>-->
     </div>
 </template>
 
@@ -39,23 +27,6 @@
         methods: {
             loadPictures: function() {
                 this.$store.dispatch('allImageFolders');
-//                var kek = client.getEntries({'content_type': 'allImageFolders'});
-//                var arrayy = ["fefe"];
-//                var i = 0;
-//                kek.then(function (response) {
-//                    // console.log(response);
-//                    // console.log(response.items[0].fields.imageFolders);
-//                    // state.folderCount = response.items[0].fields.imageFolders.length;
-//                    response.items[0].fields.imageFolders.forEach(function(folder) {
-//                        console.log(folder);
-//                        arrayy[i] = folder;
-//                        i++;
-//                    });
-//
-//                }).catch(function (error) {
-//                    console.log(error);
-//                });
-//                this.foldercount = arrayy;
             },
             getImageFolders: function() {
                 if( this.$store.getters.getFolders.items ) {
--- src/components/PictureFolder.vue
+++ src/components/PictureFolder.vue
@@ -1,18 +1,12 @@
 <template>
     <div class="picturefolder">
-        <!--<h1>Pics</h1>-->
-        <!--<br>-->
-        <!--<p>Id: {{ kek }}</p>-->
-        <div v-if="lel != null">
+        <div v-if="folder != null">
             <div class="picturefolder__container-title">
-                <h1 class="picturefolder__title" id="lel">{{ lel.fields.title }}</h1>
-                <p class="picturefolder__subtitle">{{ lel.fields.description }}</p>
-            <!--<p>{{ lel.fields }}</p>-->
+                <h1 class="picturefolder__title">{{ folder.fields.title }}</h1>
+                <p class="picturefolder__subtitle">{{ folder.fields.description }}</p>
             </div>
-            <div v-for="(image, index) in lel.fields.images">
-                <!--<div> {{image.fields.file.url }}</div>-->
+            <div v-for="(image, index) in folder.fields.images">
                 <div class="picturefolder__image-container">
-                <!--<div class="picturefolder__image-container" :id="'picturefolder' + index">-->
                     <div class="picturefolder__imagelayout">
                         <img :src="image.fields.file.url" class="picturefolder__image">
                     </div>
@@ -20,27 +14,14 @@
             </div>
         </div>
         <div v-else>loading...</div>
-        <!--<div>{{ lel }}</div>-->
-        <!--<div v-on:click="go">wot</div>-->
-        <!--<br>-->
-        <!--<p>{{ getFolders }}</p>-->
         <router-link to="/">
             <button class="picturefolder__close">+</button>
         </router-link>
-        {{ example() }}
-        <!--{{ loadClass() }}-->
+        {{ createFolder() }}
     </div>
 </template>
 
 <script>
-//    $(function() {
-//            $('.picturefolder__container-title').addClass("nope");
-//            for(var div in x) {
-//                div.addClass('nope');
-//            }
-//            debugger;
-//    });
-
     import { mapGetters } from 'vuex';
     import { mapMutations } from 'vuex';
     import { mapActions } from 'vuex';
@@ -48,16 +29,10 @@
     export default {
         data() {
             return {
-                kek: this.$route.params.id,
-                lel: null,
+                folder: null,
             }
         },
         methods: {
-//            go: function() {
-//                var imageFolders = this.$store.getters.getFolders;
-//                this.lel = "wfwf";
-//                this.lel = imageFolders.length;
-//            },
             loadPictures: function() {
                 this.$store.dispatch('allImageFolders');
             },
@@ -74,37 +49,23 @@
                 }
                 return "Nope"
             },
-            example: function() {
+            createFolder: function() {
                 var id = this.$route.params.id;
 
                 if( this.$store.getters.getFolders.items ) {
                     var imageFolders = this.$store.getters.getFolders.items[0].fields.imageFolders;
                 }
 
-                this.lel = null;
+                this.folder = null;
 
                 if( id > imageFolders.length || id < 1 || isNaN(id)) {
                     this.$router.push("/contact");
                 }
-                this.lel = this.getImageFolder(id);
+                this.folder = this.getImageFolder(id);
             },
             loadClass: function () {
-//                if( this.$store.getters.getFolders.items ) {
-//                    var imageFolders = this.$store.getters.getFolders.items[0].fields.imageFolders;
-//                }
                 var folders = this.getImageFolders();
-//                debugger;
-//                var x = folders.length;
 
-//                folders.forEach(function(i) {
-//                    var g = i;
-//                    debugger;
-//                    $('#picturefolder' + i).waypoint(function () {
-//                        $('#picturefolder' + i).addClass('slidein-right');
-//                    }, {
-//                        offset: '50%'
-//                    });
-//                });
                 for (let i=0; i<=folders.length; i++) {
                         $('#picturefolder' + i).waypoint(function () {
                             $('#picturefolder' + i).addClass('slidein-bottom');
@@ -116,17 +77,6 @@
         },
         mounted: function() {
             this.loadPictures();
-//            var id = this.$route.params.id;
-//            if( this.$store.getters.getFolders.items ) {
-//                var imageFolders = this.$store.getters.getFolders.items[0].fields.imageFolders;
-//            }
-//            this.lel = "wfwf";
-//            this.lel = imageFolders;
-//            var lol = (imagefolders = id);
-//            debugger;
-//            if(imageFolders.length = id ) {
-//                this.$router.push("/contact");
-//            }
         },
         computed: {
             ...mapGetters([
@@ -138,33 +88,6 @@
                 $(this).scrollTop(0);
             });
             this.loadPictures();
-//            var id = this.$route.params.id;
-//            if( this.$store.getters.getFolders.items ) {
-//                var imageFolders = this.$store.getters.getFolders.items[0].fields.imageFolders;
-//            }
-//            this.lel = "wfwf";
-//            this.lel = imageFolders;
-//            if( id == imageFolders.length) {
-//                this.$router.push("/contact");
-////            var id = this.$route.params.id;
-////            var imageFolders = this.getImageFolders();
-////            this.lel = "wfwf";
-////            this.lel = imageFolders;
-////            if( id == imageFolders.length) {
-////                this.$router.push("/contact");
-//            }
-
-//            var id = this.$route.params.id;
-//            var imageFolders = this.getImageFolders();
-//            this.lel = "wfwf";
-//            this.lel = imageFolders.length;
-//            this.lel = this.$route.params.id;
-//            var id = this.$route.params.id;
-//            var imageFolders = this.$store.getters.getFolders();
-//            this.lel = imageFolders.length;
-//            if( id <= imageFolders.length) {
-//                this.$router.push("/contact");
-//            }
         },
     }
 </script>
